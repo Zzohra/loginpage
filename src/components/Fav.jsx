5452cc831f62ae25d959080558672ab1017158e1
unknown <zohraa.961@gmail.com>
2023-11-26 23:39:05 +0530

    idk

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -1,59 +1,35 @@
 import { useState } from "react"
-const favs=[
-  {
-    icon:'youtube.png',
-    title:'youtube'
-    
-  },
-  {
-    icon:'youtube.png',
-    title:'twitter'
-  },
-  {
-    icon:'youtube.png',
-    title:'insta'
-  },
-  {
-    icon:'youtube.png',
-    title:'anime'
-  },
-  {
-    icon:'youtube.png',
-    title:'msn'
-  }
-]
+
 export default function Fav(props){
-  // const friends=['zohra0','zohra1','zohra2']
-  // friends.push('zohra3')
-  // console.log(friends)
- 
-  // const newFriends=friends.map((name)=>{
-  //   return `pretty ${name}`
-  // })
-  // console.log(newFriends)
-  const [items,setItems]=useState(['eggs','vegetables'])
+
+   const [input, setInput]=useState('')
+    const [items,setItems]=useState(['eggs','veggies'])
+    // onChange={(event)=>{
+    //   setColor(event.target.value)   setItems([...items, inputText])
+   function adder(){
+    setItems([...items, input])
+    setInput('')
+   }
+
   return(
     <>
-    <div className="ml-5">
-    {/* {
-  favs.map((obj)=>{
-    return (<div>
-      <div>
-       { obj.icon}
-      </div>
-      <div>
-        {obj.title}
-      </div>
-    </div>)
-  })
-} */}
-<input type="text" className="border-2 border-red-700 ml-5 rounded" />
-<button>ADD</button>
+
+    <div className="ml-5">  
+<input type="text" className="border-2 border-red-700 ml-5 rounded" 
+ onChange={(event)=>{setInput(event.target.value)}} />
+
+<button onClick={adder} >ADD</button>
+
+<ul>
+  {items.map((item)=>{ return(
+<li>
+  {item}
+</li>
+  )})}
+</ul>
 
     </div>
-    {/* <h1>hey {props.username}</h1>
-    <h1>your favourite color is {props.color}</h1>
-    <h1>your favourite food is {props.food}</h1> */}
+
 
     </>
   )
