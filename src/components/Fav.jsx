335e77de4d5290d5b4d97954207ad5d905bbdcc1
Mohammed Ameer <mohdameer788@gmail.com>
2023-11-27 19:48:37 +0530

    delete feature

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react"
-
+import {Bs0Square, BsTrash} from 'react-icons/bs'
 export default function Fav(props){
-
+  // const arr=[2,3,4]
+  // console.log("this is before pushing",arr)
+  // arr.push(5)
+  // console.log("this is after pushing",arr)
    const [input, setInput]=useState('')
-    const [items,setItems]=useState(['eggs','veggies'])
+   const [testInput,setTestUnput]=useState('')
+   const [items,setItems]=useState([
+    {
+      name:'eggs',
+      id:1
+    },
+    {
+      name:"veggies",
+      id:2
+    }
+   ])
     // onChange={(event)=>{
     //   setColor(event.target.value)   setItems([...items, inputText])
    function adder(){
     setItems([...items, input])
+
     setInput('')
    }
 
@@ -15,15 +29,15 @@ export default function Fav(props){
     <>
 
     <div className="ml-5">  
-<input type="text" className="border-2 border-red-700 ml-5 rounded" 
- onChange={(event)=>{setInput(event.target.value)}} />
-
+<input value={input} type="text" className="border-2 border-red-700 ml-5 rounded" 
+ onChange={(e)=>{setInput(e.target.value)}} />
+{/* <textarea value={} className="border-2 border-black"></textarea> */}
 <button onClick={adder} >ADD</button>
 
 <ul>
   {items.map((item)=>{ return(
-<li>
-  {item}
+<li className="flex gap-2 cursor-pointer  items-center">
+  {item.name} <button><BsTrash/></button>
 </li>
   )})}
 </ul>
