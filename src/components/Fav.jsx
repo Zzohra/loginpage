757972e447fd20cf6dbe81dbacee205323dba662
unknown <zohraa.961@gmail.com>
2023-11-29 02:20:55 +0530

    wallinthepark

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -5,13 +5,10 @@ export default function Fav(props){
   // console.log("this is before pushing",arr)
   // arr.push(5)
   // console.log("this is after pushing",arr)
+
    const [input, setInput]=useState('')
-
-   const [editId,setEditId]=useState()
-   const [testInput,setTestUnput]=useState('')
-   const cuto=[1,2,3,4,6,7,8,9]
-
-  console.log( cuto[cuto.length-1])
+   const [editId,setEditId]=useState('')
+ const [update, setUpdate]=useState('')
    const [items,setItems]=useState([
     {
       name:'eggs',
@@ -31,10 +28,21 @@ export default function Fav(props){
 
     setItems([...items,{name:input,id:newId}])
 
-    setInput('')
+  
    }
 function deleter(id){
   setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+}
+
+function updater(){
+  setItems((prevItems)=> prevItems.map((item)=>if(item.id===editId){
+    return {...item, name:update}
+    }
+    else { 
+      return
+      }
+    ))
+  
 }
 //.map()  return an array, it can onli be used on an array
 //.filter() return an array too, and it can also be onli used on an array
@@ -46,21 +54,36 @@ function deleter(id){
 <div className="flex gap-2">
 <input value={input} type="text" className="border-2 border-red-700 ml-5 rounded" 
  onChange={(e)=>{setInput(e.target.value)}} />
-{/* <textarea value={} className="border-2 border-black"></textarea> */}
-<button className="text-white bg-blue-500 p-2 rounded-md" onClick={adder} >ADD</button>
+{/* <textarea value={} className="border-2 border-black"></textarea> */} 
+
+
+
+
+<button className="text-white bg-blue-500 p-2 rounded-md" onClick={()=>{
+if(input===""){
+return
+}else{
+adder()
+}
+}} >ADD</button>
 </div>
 
 <ul>
   {items?.map((item)=>{ return(
 <li className="flex gap-2 cursor-pointer mb-2 items-center" key={item.id}>
-  {(editId===item.id) ? <input className="border-2 border-black" type="text" />: <div>{item.name} </div>}
+
+  {(editId===item.id) ? <input className="border-2 border-black" type="text" value={update} onChange={(e)=>{setUpdate(e.target.value)}} />: <div >{item.name} </div>}
+
+
+
   <button onClick={()=>deleter(item.id)}><BsTrash/></button>
   <button 
   onClick={()=>{
     setEditId(item.id)
+    setUpdate(item.name)
   }}
-  className="p-1 bg-blue-800 rounded-md text-white">Edit</button>
-{(editId===item.id) && <button onClick={()=>setEditId('ameer')} className="p-1 bg-red-600 text-white rounded-md">Done</button>}
+  className="p-1 bg-blue-800 rounded-md text-white" >Edit</button>
+{(editId===item.id) && <button onClick={()=>updater()}   className="p-1 bg-red-600 text-white rounded-md">Done</button>}
 </li>
   )})}
 </ul>
